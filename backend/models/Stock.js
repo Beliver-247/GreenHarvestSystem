31625854832bde9023573940c3e4b84939e2f7be
SenathPerera <senath.srp@gmail.com>
2024-09-20 07:40:16 +0530

    modified add and display stocks

--- backend/models/Stock.js
+++ backend/models/Stock.js
@@ -6,28 +6,33 @@ const stockSchema = new mongoose.Schema({
         enum: ['Carrot', 'Leeks', 'Cabbage', 'Potato'],
         required: true
     },
-
     qualityGrade: {
         type: String,
         enum: ['A', 'B', 'C'],
         required: true
     },
-
     batchNumber: {
         type: String,
         required: true
     },
-
     quantity: {
         type: Number,
         required: true
     },
-
     expDate: {
         type: Date,
         required: true
     },
-})
+    totalQuantity: {
+        type: Number,
+        default: 0
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+});
 
 const Stock = mongoose.model("Stock", stockSchema);
 module.exports = Stock;
