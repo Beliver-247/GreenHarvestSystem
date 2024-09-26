b70f077d1d8200653798a8acc28255ad7bd8258b
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2024-09-26 13:24:39 +0530

    Update App.js with vehicle fleet Layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import SideNav from './components/SideNav';
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LayoutVFManager from './components/LayoutVFManager';
+import LayoutDriver from './components/LayoutDriver';
 
 function App() {
   return (
