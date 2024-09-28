d5fd66c8fd2b4949864402ffb45aea0c1b991608
Dilukshan <Dviyapury@gmail.com>
2024-09-28 13:18:41 +0530

    Updated frontend with tailwind css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { store,persistor } from './redux/store.js';
+import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <PersistGate persistor={persistor}>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  </PersistGate>
 );
 
+reportWebVitals();
